e3513c708722f6a73cb9b3f802242495eee83047
iara.vsp <iaravsp@al.insper.edu.br>
2023-11-24 10:10:37 -0300

    js

--- javascript/telas.js
+++ javascript/telas.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
+    var caixa_de_texto = document.querySelector("input[type='text']");
+    var lista = document.querySelector(".chat");
 
-    submit = document.querySelectorAll("input")[1];
-    caixa_de_texto = document.querySelectorAll("input")[0]
-    lista = document.querySelector(".chat")
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    submit.addEventListener('click', function(event) {
-        novo_item = document.createElement("li")
-        novo_item.innerHTML = caixa_de_texto.value
+        var novo_item_usuario = document.createElement("li");
+        var messageContainerUsuario = document.createElement("div");
+        messageContainerUsuario.className = "message_container user_message";
+        messageContainerUsuario.innerHTML = caixa_de_texto.value;
 
-        lista.appendChild(novo_item)
-        caixa_de_texto.value = ''
-        event.preventDefault()
+        novo_item_usuario.appendChild(messageContainerUsuario);
+        lista.appendChild(novo_item_usuario);
+
+        var resposta = obterResposta(caixa_de_texto.value);
+        var novo_item_resposta = document.createElement("li");
+        var messageContainerBot = document.createElement("div");
+        messageContainerBot.className = "message_container bot_message";
+        messageContainerBot.innerHTML = resposta;
+
+        novo_item_resposta.appendChild(messageContainerBot);
+        lista.appendChild(novo_item_resposta);
+
+        caixa_de_texto.value = '';
     });
-})
+
+    function obterResposta(mensagemUsuario) {
+        return 'Oii, tem alg dúvida?';
+    }
+});
